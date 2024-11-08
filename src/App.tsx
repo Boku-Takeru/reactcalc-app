0882bd8bc3545d7cb60ea32737c4a9e644c6e757
Boku-Takeru <free0piman@gmail.com>
2024-11-09 07:14:41 +0900

    wip:html追記とstyle-componentsのinst

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import logo from './logo.svg';
+import styled from "styled-components";
+import "./App.css";
 
-function App() {
+function App(this: any) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="AllBox">
+      <input type="text" className="DisplayLayout" value={"display"} disabled />
+      <div className="InputForm">
+        <button onClick={this.click} data-num="7">
+          7
+        </button>
+        <button onClick={this.click} data-num="8">
+          8
+        </button>
+        <button onClick={this.click} data-num="9">
+          9
+        </button>
+        <button onClick={this.handleOperator} data-num="/">
+          ÷
+        </button>
+        <button onClick={this.click} data-num="4">
+          4
+        </button>
+        <button onClick={this.click} data-num="5">
+          5
+        </button>
+        <button onClick={this.click} data-num="6">
+          6
+        </button>
+        <button onClick={this.handleOperator} data-num="*">
+          ×
+        </button>
+        <button onClick={this.click} data-num="1">
+          1
+        </button>
+        <button onClick={this.click} data-num="2">
+          2
+        </button>
+        <button onClick={this.click} data-num="3">
+          3
+        </button>
+        <button onClick={this.handleOperator} data-num="-">
+          -
+        </button>
+        <button onClick={this.click} data-num="0">
+          0
+        </button>
+        <button onClick={this.clearKey} data-num="C">
+          C
+        </button>
+        <button onClick={this.equalKey} data-num="=">
+          =
+        </button>
+        <button onClick={this.handleOperator} data-num="+">
+          +
+        </button>
+      </div>
     </div>
   );
 }
