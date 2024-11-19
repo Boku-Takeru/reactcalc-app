68c0738f18aaa6a33a1d696719f4f5bdd909145d
Boku-Takeru <free0piman@gmail.com>
2024-11-19 12:02:03 +0900

    wip calc計算

--- src/component/Calculator.tsx
+++ src/component/Calculator.tsx
@@ -1,47 +1,37 @@
 import { useState } from "react";
 
 export default function Calculator() {
-  const [display, setDisplay] = useState(0);
-  const [currentInput, setCurrentInput] = useState(0);
-  const [operator, setOperator] = useState(null);
+  const [currentInput, setCurrentInput] = useState("");
+  const [result, setResult] = useState("");
 
-  function getValue(value: number ) {
+  function getValue(value: string) {
     setCurrentInput((prevInput) => prevInput + value);
   }
-
-  function handleOperator
-  // 演算子ボタンが押されたときの処理
-  // 同じ演算子の入力を防ぐ
-  function handleOperator(operator) {
-    if (currentInput !== "" && !currentInput.endsWith(operator)) {
-      currentInput += operator;
-    }
-  }
-
+  
+  
   // クリアボタンが押されたときの処理
-  function clearkey() {
-    currentInput = "";
-    updateDisplay("");
+  function clearKey(): void {
+    setCurrentInput = ("");
+    setResult = ("");
   }
-
+  
   // イコールボタンが押されたときの処理
-  function epualkey() {
+  function equalKey(equalKey:string) {
     try {
-      const result = eval(currentInput.replace("×", "*").replace("÷", "/"));
-      currentInput = result.toString();
-      updateDisplay(currentInput);
+      const result = eval(currentInput);
+      setCurrentInput = (result.tostring());
     } catch {
-      updateDisplay("Error");
-      currentInput = "";
+      setCurrentInput("Error");
+      
     }
   }
-
+  
   return (
     <form>
       {/* <!-- 液晶 --> */}
       <table>
         {/* <td colspan="4"> */}
-        <input type="text" name="display" value="" readOnly />
+        <input type="text" name="display" value={currentInput} readOnly />
         {/* </td> */}
       </table>
 
@@ -54,73 +44,73 @@ export default function Calculator() {
       >
         <tr>
           <td>
-            <input type="button" value="7" onClick={() => getValue(7)} />
+            <input type="button" value="7" onClick={() => getValue("7")} />
           </td>
           <td>
-            <input type="button" value="8" onClick={() => getValue(8)} />
+            <input type="button" value="8" onClick={() => getValue("8")} />
           </td>
           <td>
-            <input type="button" value="9" onClick={() => getValue(9)} />
+            <input type="button" value="9" onClick={() => getValue("9")} />
           </td>
           <td>
             <input
               type="button"
               value="÷"
-              onClick={() => "handleOperator('/')"}
+              onClick={() => getValue("/")}
             />
           </td>
         </tr>
         <tr>
           <td>
-            <input type="button" value="4" onClick={() => getValue(4)} />
+            <input type="button" value="4" onClick={() => getValue("4")} />
           </td>
           <td>
-            <input type="button" value="5" onClick={() => getValue(5)} />
+            <input type="button" value="5" onClick={() => getValue("5")} />
           </td>
           <td>
-            <input type="button" value="6" onClick={() => getValue(6)} />
+            <input type="button" value="6" onClick={() => getValue("6")} />
           </td>
           <td>
             <input
               type="button"
               value="×"
-              onClick={() => "handleOperator('×')"}
+              onClick={() => getValue("*")}
             />
           </td>
         </tr>
         <tr>
           <td>
-            <input type="button" value="1" onClick={() => getValue(1)} />
+            <input type="button" value="1" onClick={() => getValue("1")} />
           </td>
           <td>
-            <input type="button" value="2" onClick={() => getValue(2)} />
+            <input type="button" value="2" onClick={() => getValue("2")} />
           </td>
           <td>
-            <input type="button" value="3" onClick={() => getValue(3)} />
+            <input type="button" value="3" onClick={() => getValue("3")} />
           </td>
           <td>
             <input
               type="button"
               value="-"
-              onClick={() => "handleOperator('-')"}
+              onClick={() => getValue("-")}
             />
           </td>
         </tr>
         <tr>
           <td>
-            <input type="button" value="0" onClick={() => getValue(0)} />
+            <input type="button" value="0" onClick={() => getValue("0")} />
           </td>
           <td>
-            <input type="button" value="C" onClick="clearKey(this)" />
+            <input type="button" value="C" onClick={() => clearKey("C")} />
           </td>
           <td>
-            <input type="button" value="=" onClick="equalKey(this)" />
+            <input type="button" value="=" onClick={() => equalKey("=")} />
           </td>
           <td>
             <input
               type="button"
               value="+"
-              onClick={() => "handleOperator('+')"}
+              onClick={() => getValue("+")}
             />
           </td>
         </tr>
